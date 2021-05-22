b04f7c422dfcb78ddb35acee802b5cd5658b7426
aerox <my.aerovin@gmail.com>
2021-05-22 18:50:06 +0530

    typescript final session pipes

---
+++ pipes.ts
@@ -0,0 +1,22 @@
+//pipes is represented like "=>" 
+
+//This is used to represent the anonymous function (function without name)
+//eg:
+
+//in jasmine framework 
+
+// describe("test one", funbnction(){
+//     it("sub test one", function(){
+        
+//     })
+// })
+
+//in the latest version of javascript and typescript anonymous "function()" is 
+//represented as ()=>
+
+// describe("test one", ()=>{
+//     it("sub test one", ()=>{
+        
+//     })
+// })
+
